7c07f137503cdbce68aca0ebc4cf1feca8d39798
Николай <bgnikkov@rambler.ru>
2025-04-14 16:38:28 +0700

    Попытка получить систему максимально похожую на рабочую. Но возникает много конфликтов. Пока не решено

--- .astro/test_dag_integrity_default.py
+++ .astro/test_dag_integrity_default.py
@@ -1,4 +1,5 @@
 """Test the validity of all DAGs. **USED BY DEV PARSE COMMAND DO NOT EDIT**"""
+
 from contextlib import contextmanager
 import logging
 import os
@@ -7,9 +8,13 @@ import pytest
 
 from airflow.models import DagBag, Variable, Connection
 from airflow.hooks.base import BaseHook
+from airflow.utils.db import initdb
 
+# init airflow database
+initdb()
 
 # The following code patches errors caused by missing OS Variables, Airflow Connections, and Airflow Variables
+
 
 # =========== MONKEYPATCH BaseHook.get_connection() ===========
 def basehook_get_connection_monkeypatch(key: str, *args, **kwargs):
@@ -22,18 +27,28 @@ def basehook_get_connection_monkeypatch(key: str, *args, **kwargs):
 BaseHook.get_connection = basehook_get_connection_monkeypatch
 # # =========== /MONKEYPATCH BASEHOOK.GET_CONNECTION() ===========
 
+
 # =========== MONKEYPATCH OS.GETENV() ===========
-def os_getenv_monkeypatch(key: str, *args, default=None, **kwargs):
-    print(
-        f"Attempted to fetch os environment variable during parse, returning a mocked value for {key}"
-    )
+def os_getenv_monkeypatch(key: str, *args, **kwargs):
+    default = None
+    if args:
+        default = args[0]  # os.getenv should get at most 1 arg after the key
+    if kwargs:
+        default = kwargs.get(
+            "default", None
+        )  # and sometimes kwarg if people are using the sig
+
+    env_value = os.environ.get(key, None)
+
+    if env_value:
+        return env_value  # if the env_value is set, return it
     if (
         key == "JENKINS_HOME" and default is None
     ):  # fix https://github.com/astronomer/astro-cli/issues/601
         return None
     if default:
-        return default
-    return "NON_DEFAULT_OS_ENV_VALUE"
+        return default  # otherwise return whatever default has been passed
+    return f"MOCKED_{key.upper()}_VALUE"  # if absolutely nothing has been passed - return the mocked value
 
 
 os.getenv = os_getenv_monkeypatch
@@ -50,12 +65,15 @@ class magic_dict(dict):
         return {}.get(key, "MOCKED_KEY_VALUE")
 
 
-def variable_get_monkeypatch(key: str, default_var=None, deserialize_json=False):
+_no_default = object()  # allow falsey defaults
+
+
+def variable_get_monkeypatch(key: str, default_var=_no_default, deserialize_json=False):
     print(
         f"Attempted to get Variable value during parse, returning a mocked value for {key}"
     )
 
-    if default_var:
+    if default_var is not _no_default:
         return default_var
     if deserialize_json:
         return magic_dict()
@@ -82,7 +100,7 @@ def suppress_logging(namespace):
 
 def get_import_errors():
     """
-    Generate a tuple for import errors in the dag bag
+    Generate a tuple for import errors in the dag bag, and include DAGs without errors.
     """
     with suppress_logging("airflow"):
         dag_bag = DagBag(include_examples=False)
@@ -90,16 +108,34 @@ def get_import_errors():
         def strip_path_prefix(path):
             return os.path.relpath(path, os.environ.get("AIRFLOW_HOME"))
 
-        # we prepend "(None,None)" to ensure that a test object is always created even if its a no op.
-        return [(None, None)] + [
-            (strip_path_prefix(k), v.strip()) for k, v in dag_bag.import_errors.items()
-        ]
+        # Initialize an empty list to store the tuples
+        result = []
+
+        # Iterate over the items in import_errors
+        for k, v in dag_bag.import_errors.items():
+            result.append((strip_path_prefix(k), v.strip()))
+
+        # Check if there are DAGs without errors
+        for file_path in dag_bag.dags:
+            # Check if the file_path is not in import_errors, meaning no errors
+            if file_path not in dag_bag.import_errors:
+                result.append((strip_path_prefix(file_path), "No import errors"))
+
+        return result
 
 
 @pytest.mark.parametrize(
-    "rel_path,rv", get_import_errors(), ids=[x[0] for x in get_import_errors()]
+    "rel_path, rv", get_import_errors(), ids=[x[0] for x in get_import_errors()]
 )
 def test_file_imports(rel_path, rv):
     """Test for import errors on a file"""
-    if rel_path and rv:  # Make sure our no op test doesn't raise an error
+    if os.path.exists(".astro/dag_integrity_exceptions.txt"):
+        with open(".astro/dag_integrity_exceptions.txt", "r") as f:
+            exceptions = f.readlines()
+    print(f"Exceptions: {exceptions}")
+    if (rv != "No import errors") and rel_path not in exceptions:
+        # If rv is not "No import errors," consider it a failed test
         raise Exception(f"{rel_path} failed to import with message \n {rv}")
+    else:
+        # If rv is "No import errors," consider it a passed test
+        print(f"{rel_path} passed the import test")
--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,20 @@
 FROM quay.io/astronomer/astro-runtime:8.4.0
 
+# Install Kerberos and other system dependencies
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends \
+        krb5-user \
+        libkrb5-dev && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+# Удаляем все предустановленные пакеты
 USER root
-RUN apt-get update && apt-get install -y python3.9 python3.9-dev
+RUN pip freeze | xargs pip uninstall -y
+
+# Устанавливаем только нужные пакеты
+COPY requirements.txt .
+RUN pip install --upgrade pip && \
+    pip install --no-cache-dir --root-user-action=ignore -r requirements.txt
+
 USER astro
